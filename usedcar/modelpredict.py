0160a657984b23e011e7e6552d4450f4f50e5a3e
Manaranjan Pradhan <manaranjan@enablecloud.com>
2021-10-21 14:05:31 +0530

    Update modelpredict.py

--- usedcar/modelpredict.py
+++ usedcar/modelpredict.py
@@ -7,16 +7,8 @@ import joblib
 import warnings
 from io import BytesIO
 import requests
+import CarPredictionModel
 warnings.filterwarnings('ignore')
-
-class CarPredictionModel():
-    
-    def __init__(self, pipeline, all_features, cat_features, num_features, rmse):
-        self.pipeline = pipeline
-        self.all_features = all_features
-        self.cat_features = cat_features
-        self.num_features = num_features
-        self.rmse = rmse
 
 model_path = 'https://github.com/manaranjanp/usedcarprice/blob/main/usedcar/carmodel.pkl?raw=true'
         
