2ca07a9c708e1c1011bbf5a63f6f3d483aad91ff
Manaranjan Pradhan <manaranjan@enablecloud.com>
2021-10-21 13:52:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='usedcar',
-    version='0.0.6',
+    version='0.0.7',
     author='Manaranjan Pradhan',
     author_email='manaranjan@gmail.com',
     description='Predicting the price of an used car',
