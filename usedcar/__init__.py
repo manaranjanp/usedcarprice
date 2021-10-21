2f18cfa21f0295ffce6558b101ca1220b369ce6e
Manaranjan Pradhan <manaranjan@enablecloud.com>
2021-10-21 13:58:53 +0530

    Update __init__.py

--- usedcar/__init__.py
+++ usedcar/__init__.py
@@ -1,6 +1,6 @@
 # This is so that you can import ppack or import average from ppack
 # in stead of from ppack.functions import average
 
-from .modelpredict import UsedcarPricePredictor
+from .modelpredict import CarPredictionModel, UsedcarPricePredictor
 
 
