1976cca7915d32d0e1625feacbb9c862963cfcff
Manaranjan Pradhan <manaranjan@enablecloud.com>
2021-10-21 12:31:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='usedcarprice',
+    version='0.0.1',
+    author='Manaranjan Pradhan',
+    author_email='manaranjan@gmail.com',
+    description='Predicting the price of an used car',
+    url='https://github.com/manaranjanp/usedcarprice',
+    license='MIT',
+    packages=['usedcarprice'],
+    install_requires=['xgboost>=0.90', 'sklearn>=0.22.2'],
+)
